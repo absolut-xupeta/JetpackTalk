e17bfbe02340ab2b2d40b03fbaa5b900772c6da2
Alex <alexandre.parra@premiersoft.net>
2021-11-19 15:11:09 -0300

    feat: custom shape

--- app/src/main/java/com/example/jetpacktalk/ui/view/LazyExample.kt
+++ app/src/main/java/com/example/jetpacktalk/ui/view/LazyExample.kt
@@ -1,31 +1,35 @@
 package com.example.jetpacktalk.ui.view
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.*
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpacktalk.R
-import com.example.jetpacktalk.ui.theme.Purple700
-import com.example.jetpacktalk.ui.theme.Teal200
-import com.example.jetpacktalk.ui.theme.Typography
+import com.example.jetpacktalk.ui.theme.*
 
 @Composable
 fun LazyExample() {
     val numberList by remember {
-        mutableStateOf((1..10).toList())
+        mutableStateOf((1..20).toList())
     }
 
-    LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         item {
             Text(
-                text = stringResource(id = R.string.im_static),
+                text = stringResource(id = R.string.single_item),
                 style = Typography.h1,
                 color = Purple700
             )
@@ -34,12 +38,27 @@ fun LazyExample() {
         items(numberList) { number ->
             Spacer(modifier = Modifier.padding(top = 15.dp))
 
-            Text(text = "$number", style = Typography.body1, color = Teal200)
-
-            if (number == 10) {
-                Text(text = "---------------", style = Typography.body1)
-            }
+            SimpleCard(number)
         }
+    }
+}
+
+@Composable
+private fun SimpleCard(number: Int) {
+    val color = if (number % 2 == 0) {
+        Purple500
+    } else {
+        Purple200
+    }
+
+    Row(modifier = Modifier
+        .padding(horizontal = 10.dp)
+        .fillMaxWidth()
+        .clip(Shapes.large)
+        .background(color = color),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(text = "$number", style = Typography.body1, color = Color.Black)
     }
 }
 
