206938766c69abdce2d4ed93a4f11a4c6d539836
Alex <alexandre.parra@premiersoft.net>
2021-11-19 08:56:56 -0300

    feat: screen navigation code constants

---
+++ app/src/main/java/com/example/jetpacktalk/util/Screens.kt
@@ -0,0 +1,9 @@
+package com.example.jetpacktalk.util
+
+object Screens {
+    const val HOME = "home"
+    const val CLICK_ME = "clickMe"
+    const val LAZY_EXAMPLE = "lazyExample"
+    const val SCAFFOLD_EXAMPLE = "scaffoldExample"
+    const val STYLE_EXAMPLE = "styleExample"
+}
