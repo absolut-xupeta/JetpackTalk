4992f7a2c6bc13735d4bb8c745c420d9d6ba08c5
Alex <alexandre.parra@premiersoft.net>
2021-11-19 08:56:41 -0300

    feat: navigation scheme

--- app/src/main/java/com/example/jetpacktalk/MainActivity.kt
+++ app/src/main/java/com/example/jetpacktalk/MainActivity.kt
@@ -3,37 +3,32 @@ package com.example.jetpacktalk
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.jetpacktalk.ui.theme.JetpackTalkTheme
+import com.example.jetpacktalk.ui.view.ClickMe
+import com.example.jetpacktalk.ui.view.LazyExample
+import com.example.jetpacktalk.ui.view.Resources
+import com.example.jetpacktalk.util.Screens
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            JetpackTalkTheme {
-                // A surface container using the 'background' color from the theme
+            val navController = rememberNavController()
 
-                Greeting("Android")
-
+            NavHost(navController = navController, startDestination = Screens.HOME) {
+                composable(Screens.HOME) { Home(navController = navController) }
+                composable(Screens.STYLE_EXAMPLE) { Resources() }
+                composable(Screens.CLICK_ME) { ClickMe() }
+                composable(Screens.LAZY_EXAMPLE) { LazyExample() }
+                composable(Screens.SCAFFOLD_EXAMPLE) { LazyExample() }
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackTalkTheme {
-        Greeting("Android")
-    }
-}
-
